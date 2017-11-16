ea386ccbbdbeb03c4fb35cbafed70a76a34c667a
Varun Sharma <varunsharmadps@gmail.com>
2017-11-15 19:36:32 -0500

    Got w2v embeddings

--- src/Models/logistic_regression.py
+++ src/Models/logistic_regression.py
@@ -1,3 +1,22 @@
+from __future__ import division
+import sys
+import json
+import math
+import os
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import linear_model, datasets
+from collections import defaultdict
+import operator
+
+
+def load_word2vec(filename):
+    # Returns a dict containing a {word: numpy array for a dense word vector} mapping.
+    # It loads everything into memory.
+
+    w2vec = {}
+    with open(filename, "r") as f_in:
+        for line in f_in:
+            line_split = line.replace("\n", "").split()
+            w = line_split[0]
+            vec = np.array([float(x) for x in line_split[1:]])
+            w2vec[w] = vec
+    return w2vec
